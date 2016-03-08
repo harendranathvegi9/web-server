c05a336bcf9b742adc51bf746675e3ac26cbe926
Ravali Vemuganti <ravali178@gmail.com>
2016-03-08 13:01:59 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app=express();
+var PORT = 3000;
+
+// app.get('/',function(req,res){
+// 	res.send('Hello express!');
+
+// });
+
+var middleware={
+	requireAuthentication: function(req,res,next){
+		//console.log('private route hit');
+		next();
+	},
+
+	logger: function(req,res,next){
+		new Date().toString();
+		console.log('Request:' +req.method+ ' ' + req.originalUrl + new Date().toString());
+		next();
+	}
+};
+
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication,function(req,res){
+	res.send('About us: We are very cool!');
+
+});
+
+
+app.use(express.static(__dirname+'/public'));
+
+//console.log(__dirname);
+
+
+
+app.listen(PORT,function(){
+	console.log('express server started on' + ' ' + PORT);
+});
